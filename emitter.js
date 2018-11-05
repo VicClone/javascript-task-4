9dc0a6627e8d0b81d3eb7d4524d09a07edb7035b
Victor <evdokimovvik@gmail.com>
2018-11-05 17:13:04 +0500

    without star

--- emitter.js
+++ emitter.js
@@ -4,13 +4,76 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-const isStar = true;
+const isStar = false;
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
+
 function getEmitter() {
+
+    const events = {};
+
+    function subsribe(event, context, handler) {
+        const arrEvents = event.split('.');
+        let fieldEvent = events;
+        for (let eventCurrent of arrEvents) {
+            if (!fieldEvent[eventCurrent]) {
+                fieldEvent[eventCurrent] = new Map();
+            }
+            if (!fieldEvent[eventCurrent].has(context)) {
+                fieldEvent[eventCurrent].set(context, {
+                    handler: handler,
+                    count: 1
+                });
+            } else {
+                const field = fieldEvent[eventCurrent].get(context);
+                field.count += 1;
+            }
+            fieldEvent = fieldEvent[eventCurrent];
+        }
+    }
+
+    function getLastFieldEvenst(arrEvents) {
+        let fieldEvent = events;
+        if (!fieldEvent[arrEvents[0]]) {
+            return undefined;
+        }
+        for (const eventCurrent of arrEvents) {
+            if (fieldEvent[eventCurrent]) {
+                fieldEvent = fieldEvent[eventCurrent];
+            }
+        }
+
+        return fieldEvent;
+    }
+
+    function getCorrectFieldForEmit(arrEvents) {
+        let fieldEvent = events;
+        if (!fieldEvent[arrEvents[0]]) {
+            return undefined;
+        }
+        for (const eventCurrent of arrEvents) {
+            if (fieldEvent[eventCurrent]) {
+                fieldEvent = fieldEvent[eventCurrent];
+            } else {
+                return undefined;
+            }
+        }
+
+        return fieldEvent;
+    }
+
+    function startEmit(event) {
+        if (!event) {
+            return;
+        }
+        for (const context of event.keys()) {
+            event.get(context).handler.call(context);
+        }
+    }
+
     return {
 
         /**
@@ -18,26 +81,47 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            subsribe(event, context, handler);
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            const arrEvents = event.split('.');
+            const fieldEvent = getLastFieldEvenst(arrEvents);
+            fieldEvent.delete(context);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            const arrEvents = event.split('.');
+            while (arrEvents.length !== 0) {
+                const correctEvent = getCorrectFieldForEmit(arrEvents);
+                if (correctEvent) {
+                    startEmit(correctEvent);
+                    // console.log(events, 'after', arrEvents.length, arrEvents);
+                }
+                arrEvents.pop();
+            }
+            // console.log(correctEvent)
+            // startEmit(getLastFieldEvenst(arrEvents[0]));
+
+            return this;
         },
 
         /**
